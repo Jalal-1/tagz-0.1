b3785a204f79312248f33f578a2876d7822362ee
Jalal-1 <jalal@hmt.ai>
2023-11-06 14:07:22 +0000

    Add Vercel Analytics to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Providers from './providers/providers'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Providers>{children}</Providers></body>
+      <body className={inter.className}><Providers>{children}<Analytics /></Providers></body>
     </html>
   )
 }
